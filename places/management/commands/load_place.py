b6900a61acc1df3a0403fa09e4118a2ea1095cea
Денис Петровский <petrovsky.dv@gmail.com>
2021-05-13 21:55:09 +0300

    Revert "minimized the list of unique keys to search for a place"
    This reverts commit c8fdd51c

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -23,7 +23,11 @@ class Command(BaseCommand):
             review_result = response.json()
 
             place, created_place = Place.objects.get_or_create(
-                title=review_result['title']
+                title=review_result['title'],
+                lat=review_result['coordinates']['lat'],
+                lon=review_result['coordinates']['lng'],
+                description_short=review_result['description_short'],
+                description_long=review_result['description_long']
             )
 
             for number, image_url in enumerate(review_result['imgs'], start=1):
